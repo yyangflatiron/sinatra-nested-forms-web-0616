530797747639f2b7b7560acaa199d749dda0508c
yyangflatiron <yichen.yang@flatironschool.com>
2016-06-29 16:07:37 -0400

    Done.

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ module FormsLab
     end
 
     post '/pirates' do
-      @pirate = Pirate.new(params[:pirate])
+      pirate = Pirate.new(params[:pirate])
       params[:pirate][:ships].each do |ship_attributes|
         ship = Ship.new(ship_attributes)
       end
